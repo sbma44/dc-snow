c8871bc38ad6b488fb40104b942ef9f85cd720d0
Tom Lee <thomas.j.lee@gmail.com>
2016-01-22 11:37:22 -0500

    update

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var path = require('path');
 var cover = require('tile-cover');
 var queue = require('queue-async');
 var tracker = require('./lib/dc/avlTrackernew');
+var assemble = require('./lib/assemble');
+var geojson = require('./lib/geojson');
 var logger = require('fastlog')('dc-snow');
-var execSync = require('exec-sync');
 var point = require('turf-point');
 var distance = require('turf-distance');
 var dc = JSON.parse(fs.readFileSync('data/dc.geojson'));
+
 
 var limits = { min_zoom: 14, max_zoom: 14 };
 var tiles = cover.geojson(dc.features[0].geometry, limits);
@@ -17,163 +19,6 @@ var WORKING_DIR = path.normalize(process.env.WORKING_DIR || process.argv[2] || '
 
 function snowURL(date, ne, sw) {
     return 'http://snowmap.dc.gov/getData.aspx?starttime=' + date + '&endtime=' + date + '&ne_lat=' + ne[1] + '&ne_lng=' + ne[0] + '&sw_lat=' + sw[1] + '&sw_lng=' + sw[0];
-}
-
-function assemble(run, callback) {
-    logger.debug('assembling scrape results from run %d', run);
-
-    var q = queue();
-    fs.readdirSync(path.normalize(WORKING_DIR + '/scrape/')).forEach(function(f) {
-        if (parseInt(f.split('-')[0]) === run) {
-            q.defer(function(cb) {
-                fs.readFile(path.normalize(WORKING_DIR + '/scrape/' + f), function(err, data) {
-                    cb(err, [f, JSON.parse(data)]);
-                });
-            });
-        }
-    });
-
-    q.awaitAll(function(err, results) {
-        if (err) throw err;
-
-        results.forEach(function(result) {
-            var f = result[0];
-            var events = result[1];
-            var plowData = {};
-            events.forEach(function(e) {
-                if (!plowData[e.name])
-                    plowData[e.name] = [e];
-                else
-                    plowData[e.name].push(e);
-            });
-
-            // incorporate data into existing records, plow-by-plow
-            Object.keys(plowData).forEach(function(plowName) {
-                var data = {};
-
-                if (fs.existsSync(path.normalize(WORKING_DIR + '/' + plowName + '-events.json'))) {
-                    JSON.parse(fs.readFileSync(path.normalize(WORKING_DIR + '/' + plowName + '-events.json'))).forEach(function(dataItem) {
-                        data[+new Date(dataItem.time)] = dataItem;
-                    });
-                }
-
-                plowData[plowName].forEach(function(dataItem) {
-                    data[+new Date(dataItem.time)] = dataItem;
-                });
-
-                var dateKeys = Object.keys(data);
-                dateKeys.sort();
-                var outData = [];
-                dateKeys.forEach(function(dateKey) {
-                    outData.push(data[dateKey]);
-                });
-                fs.writeFileSync(path.normalize(WORKING_DIR + '/' + plowName + '-events.json'), JSON.stringify(outData, null, 2));
-            });
-
-            fs.unlinkSync(path.normalize(WORKING_DIR + '/scrape/' + f));
-        });
-
-        callback();
-    });
-}
-
-function geojson(callback) {
-    function emptyFeature(name, contractor) {
-        return {
-                    type: 'Feature',
-                    properties: {
-                        name: name,
-                        contractor: contractor,
-                        times: [],
-                        saltSegments: [],
-                        plowSegments: []
-                    },
-                    geometry: {
-                        type: 'LineString',
-                        coordinates: []
-                    }
-                };
-    }
-
-    fs.readdirSync(path.normalize(WORKING_DIR)).forEach(function(f, f_i) {
-        if (! /-events\.json/.test(f))
-            return;
-
-        fs.readFile(path.normalize(WORKING_DIR + '/' + f), function(err, data) {
-            if (err) throw err;
-            var events = JSON.parse(data);
-            var obj = {
-                type: 'FeatureCollection',
-                features: [
-                    emptyFeature(events[0].name, events[0].contractor)
-                ]
-            };
-            var lastPlow = lastSalt = [];
-            var featureOffset = 0;
-            events.forEach(function(e, i) {
-                if ((i > 0) && (distance(point([parseFloat(e.lon), parseFloat(e.lat)]), point(obj.features[0].geometry.coordinates[obj.features[0].geometry.coordinates.length - 1]), 'kilometers') > 0.5)) {
-                    if (lastPlow.length > 0) {
-                        lastPlow.push(i - featureOffset);
-                        obj.features[0].properties.plowSegments.push(lastPlow);
-                        lastPlow = [];
-                    }
-                    if (lastSalt.length > 0) {
-                        lastSalt.push(i - featureOffset);
-                        obj.features[0].properties.saltSegments.push(lastSalt);
-                        lastSalt = [];
-                    }
-                    obj.features.unshift(emptyFeature(e.name, e.contractor));
-                    featureOffset = i;
-                }
-
-                obj.features[0].geometry.coordinates.push([parseFloat(e.lon), parseFloat(e.lat)]);
-                obj.features[0].properties.times.push((new Date(e.time)).toISOString());
-                if (e.plow) {
-                    if (lastPlow.length === 0)
-                        lastPlow = [i - featureOffset];
-                }
-                else {
-                    if (lastPlow.length === 1) {
-                        lastPlow.push(i - featureOffset);
-                        obj.features[0].properties.plowSegments.push(lastPlow);
-                        lastPlow = [];
-                    }
-                }
-                if (e.salt) {
-                    if (lastSalt.length === 0)
-                        lastSalt = [i - featureOffset];
-                }
-                else {
-                    if (lastSalt.length === 1) {
-                        lastSalt.push(i - featureOffset);
-                        obj.features[0].properties.saltSegments.push(lastSalt);
-                        lastSalt = [];
-                    }
-                }
-            });
-
-            // ensure at least 2 points per linestring
-            obj.features = obj.features.filter(function(feat) {
-                return feat.geometry.coordinates.length > 1;
-            });
-
-            if (obj.features.length > 0) {
-                logger.debug('- writing GeoJSON from %s', f);
-                fs.writeFileSync(path.normalize(WORKING_DIR + '/' + obj.features[0].properties.name + '.geojson'), JSON.stringify(obj, null, 2));
-            }
-        });
-    });
-
-    callback();
-}
-
-function copy(callback) {
-    fs.readdirSync(path.normalize(WORKING_DIR)).forEach(function(f) {
-        if (/(scrape|-events\.json)/.test(f))
-            return;
-        fs.createReadStream(path.normalize(WORKING_DIR + '/' + f)).pipe(fs.createWriteStream(__dirname + '/data/plows/' + f));
-    });
-    //execSync(__dirname + '/upload.sh');
 }
 
 function scrape() {
@@ -205,12 +50,9 @@ function scrape() {
 
     q.defer(geojson);
 
-    q.defer(copy);
-
     q.awaitAll(function(err, results) {
         if (err) throw err;
-        logger.debug('waiting 180s...');
-        setTimeout(scrape, 180000);
+        logger.debug('Done scraping.')
     });
 }
 
--- run.sh
+++ run.sh
@@ -1,9 +1,49 @@
-set -ex
+set -eu
 
-pushd "$(dirname $0)"
+export WORKING_DIR='/tmp/snow'
+mkdir -p $WORKING_DIR/scrape || true
+mkdir -p $WORKING_DIR/events || true
+mkdir -p $WORKING_DIR/plows || true
+
+pushd "$(dirname $0)" > /dev/null
+
+# scrape
+# node index.js
+
+# convert to geojson
+node lib/geojson.js
+
+cp $WORKING_DIR/plows/*.geojson ./data/plows
+
+# remove empty files
+ls -lh $(dirname $0)/data/plows/ | grep '   0B ' | awk '{print $9}' | parallel -j 4 "rm $(dirname $0)/data/plows/{}"
+
+# push to github
 for f in $(dirname $0)/data/plows/*.geojson; do
     git add "$f"
 done
-git commit -m "updated as of $(date)"
+git commit -m "updated as of $(date)" || true
 git push origin master
-popd
+
+# push to AWS
+source $(dirname $0)/.aws_credentials
+for f in $(dirname $0)/data/plows/*.geojson; do
+    aws s3 cp $f s3://sbma44-dc/plows/$(basename $f)
+done
+
+# zipfile
+zip all.zip $(dirname $0)/data/plows/*.geojson
+aws s3 cp all.zip s3://sbma44-dc/plows/all.zip
+rm all.zip
+
+# build janky index
+INDEX="<!doctype html><html><head></head><body><p><iframe width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/9yaJGFdhNkU\" frameborder=\"0\" allowfullscreen></iframe></p><p><em>last updated at $(date)</em></p><ul><li><strong><a href=\"https://s3.amazonaws.com/sbma44-dc/plows/all.zip\">all.zip</a></strong></li>"
+for line in $(aws s3 ls s3://sbma44-dc/plows/ | awk '{print $4}' | grep geojson); do
+    INDEX="$INDEX <li><a href=\"https://s3.amazonaws.com/sbma44-dc/plows/$line\">$line</li>"
+done
+INDEX="$INDEX </ul></body></html>"
+echo $INDEX > index.html
+aws s3 cp index.html s3://sbma44-dc/plows/index.html
+rm index.html
+
+popd > /dev/null
