18e359e2076114324a53422ae46e88c2d993d577
Tom Lee <thomas.j.lee@gmail.com>
2016-01-22 20:42:08 -0500

    move date around

--- bin/html.js
+++ bin/html.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 
-var html = fs.readFileSync(path.normalize(__dirname + '/../data/html/header.html'));
-html +=  "<p><em>last updated at " + (new Date()).toString() + "</em></p><ul><li><strong><a href=\"https://s3.amazonaws.com/sbma44-dc/plows/all.zip\">all.zip</a></strong></li>";
+var html = fs.readFileSync(path.normalize(__dirname + '/../data/html/header.html')).replace('{{DATE}}',(new Date()).toString());
 
 files = fs.readFileSync(path.normalize(process.argv[3])).toString().split('\n');
 files.forEach(function(f) {
